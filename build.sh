535eb43fec787dea9ca727f1c803ebd38b8ce2a9
Brian Austin <brianjaustin@gmail.com>
2021-03-04 18:51:04 -0500

    Fix buildscript (2)

--- build.sh
+++ build.sh
@@ -7,6 +7,15 @@ export PORT=4796
 export NODEBIN=`pwd`/assets/node_modules/.bin
 export PATH="$PATH:$NODEBIN"
 
+# Setup secret config file.
+# From lecture notes
+# https://github.com/NatTuck/scratch-2021-01/blob/master/4550/0212/hangman/deploy.sh
+CFGD=$(readlink -f $HOME/.config/events)
+
+if [ ! -d "$CFGD" ]; then
+    mkdir -p "$CFGD"
+fi
+
 DATABASE_URL=$(cat "$CFGD/postgres")
 export DATABASE_URL
 
@@ -16,15 +25,6 @@ mix local.hex --force
 mix local.rebar --force
 mix deps.get
 mix compile
-
-# Setup secret config file.
-# From lecture notes
-# https://github.com/NatTuck/scratch-2021-01/blob/master/4550/0212/hangman/deploy.sh
-CFGD=$(readlink -f $HOME/.config/events)
-
-if [ ! -d "$CFGD" ]; then
-    mkdir -p "$CFGD"
-fi
 
 if [ ! -e "$CFGD/base" ]; then
     mix phx.gen.secret > "$CFGD/base"
